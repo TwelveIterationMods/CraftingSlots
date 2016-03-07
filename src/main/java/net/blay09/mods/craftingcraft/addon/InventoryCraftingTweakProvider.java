887107219be614ffe64a5100921c20c3a19436a7
Blay09 <blay09@hotmail.de>
2016-03-07 22:21:36 +0100

    Updated to latest API

--- src/main/java/net/blay09/mods/craftingcraft/addon/InventoryCraftingTweakProvider.java
+++ src/main/java/net/blay09/mods/craftingcraft/addon/InventoryCraftingTweakProvider.java
@@ -1,19 +1,19 @@
 package net.blay09.mods.craftingcraft.addon;
 
+import net.blay09.mods.craftingcraft.container.ContainerInventoryCrafting;
 import net.blay09.mods.craftingtweaks.api.CraftingTweaksAPI;
 import net.blay09.mods.craftingtweaks.api.DefaultProviderV2;
 import net.blay09.mods.craftingtweaks.api.TweakProvider;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
 import java.util.List;
 
-public class InventoryCraftingTweakProvider implements TweakProvider {
+public class InventoryCraftingTweakProvider implements TweakProvider<ContainerInventoryCrafting> {
 
     private DefaultProviderV2 defaultProvider = CraftingTweaksAPI.createDefaultProviderV2();
 
@@ -28,50 +28,61 @@ public class InventoryCraftingTweakProvider implements TweakProvider {
     }
 
     @Override
-    public int getCraftingGridStart(int id) {
+    public int getCraftingGridStart(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id) {
         return 0;
     }
 
     @Override
-    public int getCraftingGridSize(int id) {
+    public int getCraftingGridSize(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id) {
         return 9;
     }
 
     @Override
-    public void clearGrid(EntityPlayer entityPlayer, Container container, int id) {
-        for(int i = 0; i < 9; i++) {
+    public void clearGrid(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id, boolean forced) {
+        int start = getCraftingGridStart(entityPlayer, container, id);
+        int size = getCraftingGridSize(entityPlayer, container, id);
+        for(int i = start; i < start + size; i++) {
             container.transferStackInSlot(entityPlayer, i);
+            if(forced && container.inventorySlots.get(i).getHasStack()) {
+                entityPlayer.dropPlayerItemWithRandomChoice(container.inventorySlots.get(i).getStack(), false);
+                container.inventorySlots.get(i).putStack(null);
+            }
         }
     }
 
     @Override
-    public void rotateGrid(EntityPlayer entityPlayer, Container container, int id) {
-        defaultProvider.rotateGrid(this, id, entityPlayer, container);
+    public void rotateGrid(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id, boolean counterClockwise) {
+        defaultProvider.rotateGrid(this, id, entityPlayer, container, counterClockwise);
     }
 
     @Override
-    public void balanceGrid(EntityPlayer entityPlayer, Container container, int id) {
+    public void balanceGrid(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id) {
         defaultProvider.balanceGrid(this, id, entityPlayer, container);
     }
 
     @Override
-    public boolean canTransferFrom(EntityPlayer entityPlayer, Container container, int i, Slot slot) {
+    public void spreadGrid(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id) {
+        defaultProvider.spreadGrid(this, id, entityPlayer, container);
+    }
+
+    @Override
+    public boolean canTransferFrom(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int i, Slot slot) {
         return defaultProvider.canTransferFrom(entityPlayer, container, slot);
     }
 
     @Override
-    public boolean transferIntoGrid(EntityPlayer entityPlayer, Container container, int id, Slot slot) {
+    public boolean transferIntoGrid(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id, Slot slot) {
         return defaultProvider.transferIntoGrid(this, id, entityPlayer, container, slot);
     }
 
     @Override
-    public ItemStack putIntoGrid(EntityPlayer entityPlayer, Container container, int id, ItemStack itemStack, int index) {
+    public ItemStack putIntoGrid(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id, ItemStack itemStack, int index) {
         return defaultProvider.putIntoGrid(this, id, entityPlayer, container, itemStack, index);
     }
 
     @Override
-    public IInventory getCraftMatrix(EntityPlayer entityPlayer, Container container, int id) {
-        return container.inventorySlots.get(getCraftingGridStart(id)).inventory;
+    public IInventory getCraftMatrix(EntityPlayer entityPlayer, ContainerInventoryCrafting container, int id) {
+        return container.inventorySlots.get(getCraftingGridStart(entityPlayer, container, id)).inventory;
     }
 
     @Override
