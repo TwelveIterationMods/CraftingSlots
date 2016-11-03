3851836eb9c6d68d50acb5d77a2387873c2934e2
Blay09 <blay09@hotmail.de>
2016-11-03 14:24:12 +0100

    Register crafting category items

--- src/main/java/net/blay09/mods/craftingcraft/addon/JEICraftingCraft.java
+++ src/main/java/net/blay09/mods/craftingcraft/addon/JEICraftingCraft.java
@@ -3,9 +3,11 @@ package net.blay09.mods.craftingcraft.addon;
 import mezz.jei.api.*;
 import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
 import mezz.jei.api.recipe.transfer.IRecipeTransferRegistry;
+import net.blay09.mods.craftingcraft.CraftingCraft;
 import net.blay09.mods.craftingcraft.container.ContainerInventoryCrafting;
 import net.blay09.mods.craftingcraft.container.ContainerPortableCrafting;
 import net.blay09.mods.craftingcraft.container.ContainerStoneCraftingTable;
+import net.minecraft.item.ItemStack;
 
 import javax.annotation.Nonnull;
 
@@ -19,6 +21,12 @@ public class JEICraftingCraft extends BlankModPlugin {
         transferRegistry.addRecipeTransferHandler(ContainerStoneCraftingTable.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 10, 36);
         transferRegistry.addRecipeTransferHandler(ContainerPortableCrafting.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 10, 36);
         transferRegistry.addRecipeTransferHandler(ContainerInventoryCrafting.class, VanillaRecipeCategoryUid.CRAFTING, 0, 9, 9, 27);
+
+        registry.addRecipeCategoryCraftingItem(new ItemStack(CraftingCraft.portableCraftingTable, 1, 0), VanillaRecipeCategoryUid.CRAFTING);
+        registry.addRecipeCategoryCraftingItem(new ItemStack(CraftingCraft.portableCraftingTable, 1, 1), VanillaRecipeCategoryUid.CRAFTING);
+        registry.addRecipeCategoryCraftingItem(new ItemStack(CraftingCraft.stoneCraftingTable, 1, 0), VanillaRecipeCategoryUid.CRAFTING);
+        registry.addRecipeCategoryCraftingItem(new ItemStack(CraftingCraft.stoneCraftingTable, 1, 1), VanillaRecipeCategoryUid.CRAFTING);
+        registry.addRecipeCategoryCraftingItem(new ItemStack(CraftingCraft.craftingTableFrame), VanillaRecipeCategoryUid.CRAFTING);
     }
 
 }
