62f4a1c16c1eff5c2d50da9e1f96f1c217da0a03
blay09 <blay09@hotmail.de>
2021-12-05 02:41:03 +0100

    Update to Minecraft 1.18

--- shared/src/main/java/net/blay09/mods/craftingcraft/item/PortableCraftingItem.java
+++ shared/src/main/java/net/blay09/mods/craftingcraft/item/PortableCraftingItem.java
@@ -1,18 +1,18 @@
 package net.blay09.mods.craftingcraft.item;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.item.BalmItem;
 import net.blay09.mods.craftingcraft.container.PortableCraftingMenu;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
-public class PortableCraftingItem extends BalmItem {
+public class PortableCraftingItem extends Item {
 
     public static final String name = "portable_crafting";
 
