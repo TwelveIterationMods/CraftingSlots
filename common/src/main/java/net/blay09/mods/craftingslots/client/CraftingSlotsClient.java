8bf04ad96868a9d168b7a28d17e1bf46895961b1
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-27 23:38:58 +0100

    feat: Update to Minecraft 1.21.11

--- common/src/main/java/net/blay09/mods/craftingslots/client/CraftingSlotsClient.java
+++ common/src/main/java/net/blay09/mods/craftingslots/client/CraftingSlotsClient.java
@@ -1,10 +1,22 @@
 package net.blay09.mods.craftingslots.client;
 
+import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.client.BalmClientRegistrars;
+import net.blay09.mods.craftingslots.menu.InventoryCraftingMenu;
+import net.blay09.mods.craftingslots.menu.ModMenus;
+import net.blay09.mods.craftingslots.menu.PortableCraftingMenu;
+import net.minecraft.world.item.crafting.RecipeType;
+
+import static net.blay09.mods.craftingslots.CraftingSlots.id;
 
 public class CraftingSlotsClient {
     public static void initialize(BalmClientRegistrars registrars) {
         registrars.menuScreens(ModScreens::initialize);
         ModKeyBindings.initialize();
+
+        Balm.modSupport().recipeViewers().register(id("transfer_handlers"), registrar -> {
+            registrar.registerRecipeTransferHandler(PortableCraftingMenu.class, ModMenus.portableCrafting, RecipeType.CRAFTING, 1, 9, 10, 36);
+            registrar.registerRecipeTransferHandler(InventoryCraftingMenu.class, ModMenus.inventoryCrafting, RecipeType.CRAFTING, 1, 9, 10, 27);
+        });
     }
 }
