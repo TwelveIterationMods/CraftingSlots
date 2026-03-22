3f96ec044ef64e7d10d3c8c0b3a196e87a5c79fe
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 18:57:10 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/craftingslots/NeoForgeCraftingSlots.java
+++ neoforge/src/main/java/net/blay09/mods/craftingslots/NeoForgeCraftingSlots.java
@@ -3,12 +3,13 @@ package net.blay09.mods.craftingslots;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(CraftingSlots.MOD_ID)
 public class NeoForgeCraftingSlots {
-    public NeoForgeCraftingSlots(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeCraftingSlots(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         Balm.initializeMod(CraftingSlots.MOD_ID, context, CraftingSlots::initialize);
     }
 }
--- neoforge/src/main/java/net/blay09/mods/craftingslots/client/NeoForgeCraftingSlotsClient.java
+++ neoforge/src/main/java/net/blay09/mods/craftingslots/client/NeoForgeCraftingSlotsClient.java
@@ -5,12 +5,13 @@ import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.blay09.mods.craftingslots.CraftingSlots;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(value = CraftingSlots.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgeCraftingSlotsClient {
-    public NeoForgeCraftingSlotsClient(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeCraftingSlotsClient(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         BalmClient.initializeMod(CraftingSlots.MOD_ID, context, CraftingSlotsClient::initialize);
     }
 }
