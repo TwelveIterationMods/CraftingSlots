6b99854ae253d959221db7f15c6a99ad96931e26
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-11 19:23:21 +0200

    fix: Fix inventory crafting overlapping with recipe viewers

--- common/src/main/java/net/blay09/mods/craftingslots/client/CraftingSlotsClient.java
+++ common/src/main/java/net/blay09/mods/craftingslots/client/CraftingSlotsClient.java
@@ -2,10 +2,15 @@ package net.blay09.mods.craftingslots.client;
 
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.client.BalmClientRegistrars;
+import net.blay09.mods.balm.mixin.AbstractContainerScreenAccessor;
+import net.blay09.mods.craftingslots.client.screen.InventoryCraftingScreen;
 import net.blay09.mods.craftingslots.menu.InventoryCraftingMenu;
 import net.blay09.mods.craftingslots.menu.ModMenus;
 import net.blay09.mods.craftingslots.menu.PortableCraftingMenu;
+import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.world.item.crafting.RecipeType;
+
+import java.util.List;
 
 import static net.blay09.mods.craftingslots.CraftingSlots.id;
 
@@ -17,6 +22,15 @@ public class CraftingSlotsClient {
         Balm.modSupport().recipeViewers().register(id("transfer_handlers"), registrar -> {
             registrar.registerRecipeTransferHandler(PortableCraftingMenu.class, ModMenus.portableCrafting, RecipeType.CRAFTING, 1, 9, 10, 36);
             registrar.registerRecipeTransferHandler(InventoryCraftingMenu.class, ModMenus.inventoryCrafting, RecipeType.CRAFTING, 1, 9, 10, 27);
+            registrar.registerScreenOcclusion(InventoryCraftingScreen.class, containerScreen -> {
+                final var accessor = ((AbstractContainerScreenAccessor) containerScreen);
+                return List.of(new Rect2i(
+                        accessor.getLeftPos() + accessor.getImageWidth(),
+                        accessor.getTopPos(),
+                        40,
+                        64
+                ));
+            });
         });
     }
 }
